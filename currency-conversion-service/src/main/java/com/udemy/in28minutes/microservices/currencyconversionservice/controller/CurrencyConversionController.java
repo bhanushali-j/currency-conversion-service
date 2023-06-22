7e22f30a5e25d8aa067f5b9b6ce72c680e965fb8
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-06-22 18:24:06 +0530

    Connected currency-conversion-service with currency-exchange-service RestTemplate.

--- currency-conversion-service/src/main/java/com/udemy/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/udemy/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,18 +1,34 @@
 package com.udemy.in28minutes.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.udemy.in28minutes.microservices.currencyconversionservice.model.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
-	
+
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion convert(@PathVariable String from,@PathVariable String to, @PathVariable BigDecimal quantity) {
-		return new CurrencyConversion(10001,from,to,BigDecimal.valueOf(65.00),quantity,BigDecimal.valueOf(650.00),"8001");
+	public CurrencyConversion convert(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+		CurrencyConversion currencyConversion = (CurrencyConversion) responseEntity.getBody();
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment());
 	}
 }
