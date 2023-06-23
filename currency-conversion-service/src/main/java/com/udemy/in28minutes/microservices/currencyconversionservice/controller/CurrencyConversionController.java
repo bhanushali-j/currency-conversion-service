590e84ff5fdb23a232310cf1701c0887ced463d8
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-06-23 15:31:02 +0530

    Added feign code for intercommunication of currency-conversion and currency-exchange

--- currency-conversion-service/src/main/java/com/udemy/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/udemy/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -4,17 +4,22 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.udemy.in28minutes.microservices.currencyconversionservice.feign.CurrencyExchangeProxy;
 import com.udemy.in28minutes.microservices.currencyconversionservice.model.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
-
+	
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion convert(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
@@ -29,6 +34,17 @@ public class CurrencyConversionController {
 		CurrencyConversion currencyConversion = (CurrencyConversion) responseEntity.getBody();
 
 		return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()),
-				currencyConversion.getEnvironment());
+				currencyConversion.getEnvironment()+" rest template");
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion convertWithFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(), quantity, quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment()+" feign"); 
+		
 	}
 }
---
+++ currency-conversion-service/src/main/java/com/udemy/in28minutes/microservices/currencyconversionservice/feign/CurrencyExchangeProxy.java
@@ -0,0 +1,14 @@
+package com.udemy.in28minutes.microservices.currencyconversionservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.udemy.in28minutes.microservices.currencyconversionservice.model.CurrencyConversion;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
